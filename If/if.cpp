af4e0ef3de5c99a34fed880b61b3ed6b1e7472c4
Kevin Specht <kspecht@mitre.org>
2024-05-13 14:55:31 -0400

    if else

--- If/if.cpp
+++ If/if.cpp
@@ -11,5 +11,15 @@ int main()
     else
         std::cout << "The value is non-zero\n";
 
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+
+    if (x > 0)
+        std::cout << "The value is positive\n";
+    else if (x < 0)
+        std::cout << "The value is negative\n";
+    else
+        std::cout << "The value is zero\n";
+
     return 0;
 }
